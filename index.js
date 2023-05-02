35759429cb9899b6d39ba2e2c61175f5a330ae01
dattapolade <dattatraybpolade1506@gmail.com>
2023-05-02 17:41:44 +0530

    added weather website code

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const https = require("https")
+const bodyParser = require("body-parser");
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get("/", (req, res) => {
+    res.render("index", { t: null });
+})
+
+app.post("/", async (req, res) => {
+    try{ const key = req.body.cityName;
+        // const url="https://api.openweathermap.org/data/2.5/weather?q=Pune"&callback=test&appid=542885aff3ed68bbe340ef4a8a40ed30";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + key + "&appid=542885aff3ed68bbe340ef4a8a40ed30&units=metric"
+    
+        https.get(url, (response) => {
+            console.log(response.statusCode);
+    
+            response.on("data", (data) => {
+                const d = JSON.parse(data);
+                if(d && d.main){
+                const t = d.main.temp;
+                const f = d.weather[0].description;
+                const icon = d.weather[0].icon;
+                //    console.log(d)
+                const imagurl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+                res.render('index', { t, f, key, imagurl })
+                }
+                else{
+                    res.send("Sorry,Weather is not available for your city")
+                }
+            })
+        })
+    }
+    catch(err){
+        res.send(err);
+    } 
+})
+
+app.listen(1400, () => {
+    console.log("i am back ");
+})
